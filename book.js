75f3bea9ec401bf6da627111dccd8ca0a44341b3
Johnny Nguyen <94998830+johnly25@users.noreply.github.com>
2023-11-13 15:23:18 -0800

    add remove book function

--- book.js
+++ book.js
@@ -1,11 +1,134 @@
 const myLibrary = [];
+const addButton = document.getElementById("add-btn");
+const bookDialog = document.getElementById("bookDialog");
+const output = document.querySelector("output");
+const confirmBtn = document.getElementById("confirmBtn");
+const cancelBtn = document.getElementById("cancelBtn");
+const table = document.querySelector(".main table");
+const placeholder = new Book('Naruto', "Musashi Kishimoto", 100, true);
+const placeholder2 = new Book('One Piece', "Oda", 100, true);
+const placeholder3 = new Book('Bleach', "Kubo", 100, false);
+myLibrary.push(placeholder, placeholder2, placeholder3);
+
+
+updateTable();
+addRemoveButton();
+
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
 
-    this.info = function() {
+    this.info = function () {
         console.log(title + " by " + author + ", " + pages + " pages, " + read);
     }
 }
+
+function addBookToLibrary() {
+    const title = document.getElementById('title')
+    const author = document.getElementById('author');
+    const pages = document.getElementById('pages');
+    const status = document.getElementById('read');
+    const read = checkStatus(status.value);
+    const book = new Book(title.value, author.value, pages.value, read);
+    myLibrary.push(book);
+    clearInput();
+}
+
+function clearInput() {
+    const title = document.getElementById('title')
+    const author = document.getElementById('author');
+    const pages = document.getElementById('pages');
+    const status = document.getElementById('read');
+    title.value = '';
+    author.value = '';
+    pages.value = '';
+    status.value = 'read';
+}
+
+function updateTable() {
+    const tbody = document.querySelector("tbody");
+    tbody.textContent = '';
+
+    for (let i = 0; i < myLibrary.length; i++) {
+        const row = document.createElement('tr');
+        const title = document.createElement('td');
+        const author = document.createElement('td');
+        const pages = document.createElement('td');
+        const status = document.createElement('td');
+        const close = document.createElement('td');
+
+        title.textContent = myLibrary[i].title;
+        author.textContent = myLibrary[i].author;
+        pages.textContent = myLibrary[i].pages;
+
+        const select = document.createElement('select');
+        const option = document.createElement('option');
+        const option2 = document.createElement('option');
+        option.value = 'read';
+        option.textContent = 'Read';
+        option2.value = 'unread';
+        option2.textContent = 'Unread';
+        select.append(option);
+        select.append(option2);
+        status.append(select);
+        select.value = setStatus(myLibrary[i].read);
+        select.classList.add('read');
+
+
+        const span = document.createElement('span');
+        span.textContent = 'close';
+        span.classList.add('material-symbols-outlined');
+        span.classList.add('closeBtn');
+        span.dataset.index = i;
+        close.append(span);
+
+        row.append(title);
+        row.append(author);
+        row.append(pages);
+        row.append(status);
+        row.append(close);
+        tbody.append(row);
+    }
+
+}
+
+function setStatus(read) {
+    if (read) {
+        return 'read';
+    } else {
+        return 'unread';
+    }
+
+}
+function checkStatus(status) {
+    return status == 'read';
+}
+
+
+
+addButton.addEventListener("click", () => {
+    bookDialog.showModal();
+});
+
+cancelBtn.addEventListener("click", () => {
+    clearInput();
+});
+
+function addRemoveButton() {
+    const removeBtns = document.querySelectorAll(".closeBtn");
+    removeBtns.forEach(removeBtn => {
+        removeBtn.addEventListener('click', () => {
+            console.log("hello");
+        });
+    });
+}
+
+confirmBtn.addEventListener("click", (event) => {
+    event.preventDefault(); // We don't want to submit this fake form
+    addBookToLibrary();
+    updateTable();
+    addRemoveButton();
+    bookDialog.close(); // Have to send the select box value here.
+});
